f56ee7733e2cdb2ad2339dea377d4c29fe90faa6
ThirumuruganE <thiru20@protonmail.com>
2020-01-11 00:12:25 +0530

    Version 1.0

--- ntcp.py
+++ ntcp.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
-# GUI_for_finding_area
+# import
 from tkinter import *
 from tkinter.simpledialog import askstring, askinteger, askfloat
 from tkinter.messagebox import showerror, showinfo
 from tkinter import filedialog
 
-# import tkinter as tk
-
 root = Tk()
 root.title("EUD based NTCP calculator")
 
 
+# defining manual entry
 def ab_value():
     global alphabeta
     alphabeta = float(alpha.get())
-    # alphabeta = float(alphabeta)
 
 
 def s_value():
@@ -35,33 +33,29 @@ def clear():
 def a_value():
     global a
     a = float(abi.get())
-    # a = float(a)
 
 
 def g_value():
     global g
     g = float(gp.get())
-    # g = float(g)
 
 
 def td_value():
     global td
     td = float(tdf.get())
-    # td = float(td)
 
 
 def nofraction():
     global nof
     nof = float(no.get())
-    # nof = float(nof)
 
 
 def doseperfraction():
     global dosepf
     dosepf = float(dosep.get())
-    # dosepf = float(dosepf)
 
 
+# parameter emami et al
 def brain():
     global a, g, td, s
     a = 5
@@ -190,6 +184,7 @@ def rectum():
     td = 80
 
 
+# parameter nimierko et al
 def nbrain():
     global alphabeta, nof, dosepf, a, g, td, s
     s = 0
@@ -300,36 +295,21 @@ def nparotids():
     td = 31.4
 
 
+# ask file name
 def file_name():
     global fn
     fn = filedialog.askopenfilename()
 
 
-def dose_column():
-    global d
-    d = d.get()
-    # Try convert a str to int
-    # If unable eg. int('hello') or int('5.5')
-    # then show an error.
-    try:
-        d = int(d)
-    # ValueError is the type of error expected from this conversion
-    except ValueError:
-        # Display Error Window (Title, Prompt)
-        showerror('Non-Int Error', 'Please enter an integer')
-    else:
-        print(d)
-
-
+# calculate
 def calculate_eud():
     import pandas as pd
     import os
-    import matplotlib.pyplot as plt
 
     test = pd.read_excel(fn)
     dose = (test.loc[:, "dose"])
     volume = (test.loc[:, "volume"])
-    # plt.plot(dose,volume)
+
     totalnumber = dose.count()
     global eud, q, ntcp
     eud = float(0)
@@ -345,8 +325,10 @@ def calculate_eud():
     ntcp = ntcp ** g
     ntcp = 1.0 + ntcp
     ntcp = (1.0 / ntcp) * 100
-    showinfo('Normal tissue complication probability (percentage)', 'NTCP Percentage %s\nEUD is %s Gy' % (ntcp,q))
+    showinfo('Normal tissue complication probability (percentage)', 'NTCP Percentage %s\nEUD is %s Gy' % (ntcp, q))
 
+
+# display reference
 def open1():
     new_frame = Toplevel(root)
     new_frame.title("Reference Values")
@@ -357,53 +339,73 @@ def open1():
     scro.config(command=tex.yview)
     tex.config(yscrollcommand=scro.set)
     ent = '''Gay and Niemierko                                                     '''
-    tex.insert(END,ent)
+    tex.insert(END, ent)
     from prettytable import PrettyTable
     x = PrettyTable()
-    x.field_names = ['NTCP','fx', 'alpha/beta','dose/fx','a','gamma50','TD50']
-    x.add_row(['Brain',30,2.9,2,5,3,60])
-    x.add_row(['Brainstem',30,2.5,2,7,3,65])
-    x.add_row(['Heart',30,2.5,2,3,3,50])
-    x.add_row(['Kidney',30,2,2,1,3,28])
-    x.add_row(['Liver',30,2.5,2,3,3,40])
-    x.add_row(['Lung',30,4,2,1,2,24.5])
-    x.add_row(['Oesophagus',30,10,2,19,4,68])
-    x.add_row(['Optic Chiasm',30,2,2,25,3,65])
-    x.add_row(['Optic Nerve',30,1.6,2,25,3,65])
-    x.add_row(['Parotids',30,3,2,1,2,31.4])
-    tex.insert(END,x)
+    x.field_names = ['NTCP', 'fx', 'alpha/beta', 'dose/fx', 'a', 'gamma50', 'TD50']
+    x.add_row(['Brain', 30, 2.9, 2, 5, 3, 60])
+    x.add_row(['Brainstem', 30, 2.5, 2, 7, 3, 65])
+    x.add_row(['Heart', 30, 2.5, 2, 3, 3, 50])
+    x.add_row(['Kidney', 30, 2, 2, 1, 3, 28])
+    x.add_row(['Liver', 30, 2.5, 2, 3, 3, 40])
+    x.add_row(['Lung', 30, 4, 2, 1, 2, 24.5])
+    x.add_row(['Oesophagus', 30, 10, 2, 19, 4, 68])
+    x.add_row(['Optic Chiasm', 30, 2, 2, 25, 3, 65])
+    x.add_row(['Optic Nerve', 30, 1.6, 2, 25, 3, 65])
+    x.add_row(['Parotids', 30, 3, 2, 1, 2, 31.4])
+    tex.insert(END, x)
     emami = '''
     In emami data while calculating BED the dose per fraction is 
     considered as 1.8-2 Gy, 5 days a week                             '''
-    tex.insert(END,emami)
+    tex.insert(END, emami)
     y = PrettyTable()
-    y.field_names = ['Structure','End point','a','gamma50','TD50']
-    y.add_row(['Brain','Necrosis',5,3,60])
-    y.add_row(['Brainstem','Necrosis',7,3,65])
-    y.add_row(['Optic chiasm','Blindness',25,3,65])
-    y.add_row(['Colon','Obstruction',6,4,55])
-    y.add_row(['Ear','Acute serous otitis',31,3,40])
-    y.add_row(['Ear','Chronic serous otitis',31,4,65])
-    y.add_row(['Esophagus','Perforation',19,4,68])
-    y.add_row(['Heart','Pericarditis',3,3,50])
-    y.add_row(['Kidney','Nephritis',1,3,28])
-    y.add_row(['Lens','Cataract',3,1,18])
-    y.add_row(['Liver','Liver failure',3,3,40])
-    y.add_row(["Lung",'Pneumonitis',1,2,24.5])
-    y.add_row(['Optic nerve','Blindness',25,3,65])
-    y.add_row(['Retina','Blindness',15,2,65])
-    tex.insert(END,y)
+    y.field_names = ['Structure', 'End point', 'a', 'gamma50', 'TD50']
+    y.add_row(['Brain', 'Necrosis', 5, 3, 60])
+    y.add_row(['Brainstem', 'Necrosis', 7, 3, 65])
+    y.add_row(['Optic chiasm', 'Blindness', 25, 3, 65])
+    y.add_row(['Colon', 'Obstruction', 6, 4, 55])
+    y.add_row(['Ear', 'Acute serous otitis', 31, 3, 40])
+    y.add_row(['Ear', 'Chronic serous otitis', 31, 4, 65])
+    y.add_row(['Esophagus', 'Perforation', 19, 4, 68])
+    y.add_row(['Heart', 'Pericarditis', 3, 3, 50])
+    y.add_row(['Kidney', 'Nephritis', 1, 3, 28])
+    y.add_row(['Lens', 'Cataract', 3, 1, 18])
+    y.add_row(['Liver', 'Liver failure', 3, 3, 40])
+    y.add_row(["Lung", 'Pneumonitis', 1, 2, 24.5])
+    y.add_row(['Optic nerve', 'Blindness', 25, 3, 65])
+    y.add_row(['Retina', 'Blindness', 15, 2, 65])
+    tex.insert(END, y)
 
-'''def voxel():
-    global voxel1
-    voxel1 = voxel1.get()
-    voxel1 = float(voxel1)
 
-def volume_size():
-    global vs
-    vs = vs.get()
-    vs = float(vs)'''
+# display License
+def open2():
+    new_frame2 = Toplevel(root)
+    new_frame2.title('License')
+    scr = Scrollbar(new_frame2)
+    tex1 = Text(new_frame2, height=30, width=70)
+    scr.pack(side=RIGHT, fill=Y)
+    tex1.pack(side=LEFT, fill=Y)
+    scr.config(command=tex1.yview)
+    tex1.config(yscrollcommand=scr.set)
+    lic = '''<EUD based NTCP calculator.>
+    \nCopyright (C) <2020>  <Thirumurugan Elango>
 
+    \nThis program is free software: you can redistribute it and/or modify
+    \nit under the terms of the GNU General Public License as published by
+    \nthe Free Software Foundation, either version 3 of the License, or
+    \n(at your option) any later version.
+
+    \nThis program is distributed in the hope that it will be useful,
+    \nbut WITHOUT ANY WARRANTY; without even the implied warranty of
+    \nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    \nGNU General Public License for more details.
+
+    \nYou should have received a copy of the GNU General Public License
+    \nalong with this program.  If not, see <https://www.gnu.org/licenses/>.'''
+    tex1.insert(END, lic)
+
+
+# Entry widgets for manual inputs
 abi = Entry(root, width=25, borderwidth=5)
 abi.grid(row=0, column=1)
 gp = Entry(root, width=25, borderwidth=5)
@@ -417,6 +419,7 @@ dosep.grid(row=2, column=1)
 alpha = Entry(root, width=25, borderwidth=5)
 alpha.grid(row=2, column=3)
 
+# Create Buttons
 btn_1 = Button(root, text="import", command=file_name).grid(row=5, column=0)
 btn_2 = Button(root, text="calculate", command=calculate_eud).grid(row=5, column=1)
 btn_3 = Button(root, text="manual value entry",
@@ -424,14 +427,14 @@ btn_3 = Button(root, text="manual value entry",
                                 a_value()])
 btn_3.grid(row=5, column=3)
 btn_4 = Button(root, text="Clear", command=clear).grid(row=5, column=2)
-# btn_5 = Button(root, text="Reference Details", command=open1).grid(row=5, column=4)
+# create Label for manual entry
 l1 = Label(root, text="a value").grid(row=0, column=0)
 l2 = Label(root, text="Gamma 50 value").grid(row=0, column=2)
 l3 = Label(root, text='Tolerance Dose in Gy').grid(row=1, column=0)
 l4 = Label(root, text="Number of Fractions").grid(row=1, column=2)
 l5 = Label(root, text="Dose per faction in Gy").grid(row=2, column=0)
 l6 = Label(root, text="Alpha/Beta Value").grid(row=2, column=2)
-
+# create menubar
 menubar = Menu(root)
 root.config(menu=menubar)
 submenu = Menu(menubar, tearoff=0)
@@ -466,10 +469,13 @@ nmenu.add_command(label="Optic Chiasm", command=nopticchiasm)
 nmenu.add_command(label="Optic nerve", command=nopticnerve)
 nmenu.add_command(label="Parotids", command=nparotids)
 
-rmenu = Menu(menubar,tearoff=0)
-menubar.add_cascade(label="Reference",menu = rmenu)
-rmenu.add_command(label='Reference value',command =open1)
+rmenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Reference", menu=rmenu)
+rmenu.add_command(label='Reference value', command=open1)
 
-
+amenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label='About', menu=amenu)
+amenu.add_command(label='about', command=open2)
 
 root.mainloop()
+
